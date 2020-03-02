eb5cf82644a711d05800d6d6a52adadf1da35c6a
bobinlee <bobeen.lee@huiseoul.com>
2020-03-02 13:42:33 +0900

    add movie screen

---
+++ src/screens/MovieScreen.tsx
@@ -0,0 +1,100 @@
+import React, {Component, ComponentClass} from 'react';
+import {FlatListProps, FlatList, ListRenderItem} from 'react-native';
+import styled from 'styled-components/native';
+
+import {StackNavigationProp} from '@react-navigation/stack';
+import {MovieItem, movies} from 'src/apis/movie';
+
+interface Props {
+  navigation: StackNavigationProp<any>;
+}
+
+interface States {
+  movieItems: any[];
+}
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: #eee;
+`;
+
+const Movies = styled<ComponentClass<FlatListProps<MovieItem>>>(FlatList).attrs(
+  {
+    contentContainerStyle: {
+      paddingBottom: 95,
+      paddingHorizontal: 20
+    },
+  },
+)`
+  flex: 1;
+  width: 100%;
+`;
+
+const MovieItemView = styled.View`
+  flex-direction: row;
+  align-items: center;
+  height: 50px;
+  background-color: #eee;
+`;
+
+const ItemTitle = styled.Text`
+    flex: 3;
+`;
+
+const ItemRating = styled.Text`
+    flex: 1;
+    text-align: right;
+`;
+
+class MovieScreen extends Component<Props, States> {
+  public static open(
+    navigation: StackNavigationProp<any>,
+  ): void {
+    navigation.navigate('Movie');
+  }
+
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      movieItems: [],
+    };
+  }
+
+  public async componentDidMount() {
+    this.setState({
+      movieItems: await movies(),
+    });
+  }
+
+  public render() {
+    const {movieItems} = this.state;
+    return (
+      <Container>
+        <Movies
+          data={movieItems}
+          keyExtractor={this.movieItemKeyExtractor}
+          renderItem={this.renderMovieItem}
+        />
+      </Container>
+    );
+  }
+
+  private movieItemKeyExtractor = (item: MovieItem, index: number) => {
+    return `${item.id}${index}`;
+  };
+
+  private renderMovieItem: ListRenderItem<MovieItem> = ({item}) => {
+    const {title, rating} = item;
+    return (
+      <MovieItemView>
+        <ItemTitle>{title}</ItemTitle>
+        <ItemRating>{rating}</ItemRating>
+      </MovieItemView>
+    );
+  };
+}
+
+export default MovieScreen;
